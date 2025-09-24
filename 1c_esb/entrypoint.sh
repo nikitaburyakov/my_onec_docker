422f8fc1acd44d561e7634452c0f5803aa003131
Nikita Buryakov <e777kx43@gmail.com>
2025-09-24 10:18:10 +0300

    update 1c_esb

--- 1c_esb/entrypoint.sh
+++ 1c_esb/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 /tmp/esb/1ce-installer-cli query installed > /tmp/params.yaml
-ID=$(yq -r '.id' /tmp/params.yaml)
-VERSION=$(yq -r '.version' /tmp/params.yaml)
-ARCH=$(yq -r '.arch' /tmp/params.yaml)
+ESB_ID=$(yq -r '.id' /tmp/params.yaml)
+ESB_VERSION=$(yq -r '.version' /tmp/params.yaml)
+ESB_ARCH=$(yq -r '.arch' /tmp/params.yaml)
 
-ESB_BIN="/opt/1C/1CE/components/$ID-$VERSION-$ARCH/bin/esb"
-INSTANCE_DIR="/var/opt/1C/1CE/instances/$ID"
+export ESB_BIN=$(find $ESBDIR/components/ -name 'esb' | head -n 1)
 
-if [ ! -x "$ESB_BIN" ]; then
-  echo "Файл не найден или не исполняемый: $ESB_BIN" >&2
-  exit 1
+export INSTANCE_DIR="/var/opt/1C/1CE/instances/$ESB_ID"
+
+if [ -f $(find $INSTANCE_DIR -name "*.pid") ]; then
+    PIDFILE=$(find $INSTANCE_DIR -name "*.pid")
+    rm $PIDFILE
 fi
 
 exec "$ESB_BIN" start --instance="$INSTANCE_DIR" "$@"
